6b8527230d5b9fffe076107c00524da4710539ac
DG <mymac@mys-MacBook-Pro.local>
2023-12-16 19:23:10 +0545

    changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,8 +6,8 @@ const Home = () => {
    <div className='Menu bg-slate-800 flex justify-between text-white px-4 py-2'>
     <h1 className='text-4xl'>Logo</h1>
     <nav className='space-x-7'>
-    <NavLink to='/about-page'>About</NavLink>
-    <NavLink to='/contact'>Contact</NavLink>
+    <NavLink className='hover:text-red-300 mb-4' to='/about-page'>About</NavLink>
+    <NavLink className='hover:text-orange-800' to='/contact'>Contact</NavLink>
     </nav>
    </div>
   )
